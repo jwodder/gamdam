b1d62abd8771835e8b9267e5b664e1b5a1c7f3aa
John T. Wodder II <git@varonathe.org>
2021-10-21 13:08:15 -0400

    Copy README intro to package docstring

--- src/gamdam/__init__.py
+++ src/gamdam/__init__.py
@@ -1,6 +1,18 @@
 """
 Git-Annex Mass Downloader and Metadata-er
 
+``gamdam`` is the Git-Annex Mass Downloader and Metadata-er.  It takes a stream
+of JSON Lines describing what to download and what metadata each file has,
+downloads them in parallel to a git-annex_ repository, attaches the metadata
+using git-annex's metadata facilities, and commits the results.
+
+This program was written as an experiment/proof-of-concept for a larger
+program.  It is my first time using trio_, so there may be some oddness in the
+code.
+
+.. _git-annex: https://git-annex.branchable.com
+.. _trio: https://github.com/python-trio/trio
+
 Visit <https://github.com/jwodder/gamdam> for more information.
 """
 
